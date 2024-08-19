5344c23e2d088caa462a464c2c9de28d8fa0b4a9
Akshay Pandit <panditakshay402@gmail.com>
2024-08-20 02:32:17 +0530

    tailwind bug solved

--- app-example/(tabs)/index.tsx
+++ app-example/(tabs)/index.tsx
@@ -3,9 +3,9 @@ import {View,Text,StatusBar,StyleSheet} from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar/>
+      <StatusBar style="auto" />
     </View>
   );
 }
